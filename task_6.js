20e665423a79b2d0e50e1005052590f2f02e281d
Kirill Skok <kirskok@yahoo.com>
2023-12-30 18:57:32 +0300

    Add new task

---
+++ task_6.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+
+//Input: s = "PAYPALISHIRING", numRows = 3
+//Output: "PAHNAPLSIIGYIR"
+
+var convert = function(s, numRows) {
+    
+};
